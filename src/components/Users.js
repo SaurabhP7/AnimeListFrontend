f8e7145a7c17930ebfbc37eccc9a841ed5b871e4
YourGoodFriendSP <71482137+YourGoodFriendSP@users.noreply.github.com>
2022-01-13 08:54:08 +0530

    title changed

--- src/components/Users.js
+++ src/components/Users.js
@@ -14,7 +14,7 @@ const Users=() => {
       <h4>Users</h4>
       <Row style={{ marginTop:'30px' }}>
         <Col>
-          <div style={{ flex:'display', alignItems:'center' }}>
+          <div style={{ flex:'display', justifyContent:'center' }}>
             <Card style={{ width:'300px' }} elevation={6}>
               <img src={user} width="300" />
             </Card>
